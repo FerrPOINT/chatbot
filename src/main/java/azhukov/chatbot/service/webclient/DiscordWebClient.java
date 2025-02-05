5d8f74436a2c709be5a1f4b3b6ba00871bd5f4d6
azhukov <azhukov@wm-team.ru>
2025-02-05 22:39:14 +0700

    введение прокси

--- src/main/java/azhukov/chatbot/service/webclient/DiscordWebClient.java
+++ src/main/java/azhukov/chatbot/service/webclient/DiscordWebClient.java
@@ -4,6 +4,8 @@ import azhukov.chatbot.dto.ChatRequest;
 import azhukov.chatbot.dto.ChatResponse;
 import azhukov.chatbot.service.CommonChatService;
 import azhukov.chatbot.service.MappingService;
+import com.neovisionaries.ws.client.WebSocket;
+import com.neovisionaries.ws.client.WebSocketFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
@@ -17,11 +19,14 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.Compression;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
-import org.springframework.beans.factory.annotation.Value;
+import okhttp3.OkHttpClient;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -30,28 +35,43 @@ public class DiscordWebClient {
 
     private final MappingService mappingService;
     private final CommonChatService commonChatService;
+    private final DiscordProperties properties;
 
-    @Value("${discord.token:disabled}")
-    private String token;
     private JDA jda;
 
     @PostConstruct
     void init() {
-        if ("disabled".equals(token)) {
+        if ("disabled".equals(properties.getToken())) {
             return;
         }
-        jda = JDABuilder.createDefault(token)
+
+        // Настройка прокси (замените адрес и порт на свои)
+        Proxy proxy = properties.getProxyHost() == null ? Proxy.NO_PROXY : new Proxy(Proxy.Type.SOCKS, new InetSocketAddress(properties.getProxyHost(), properties.getProxyPort()));
+
+        // Создаём OkHttpClient с настройками прокси
+        OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
+        httpClientBuilder.proxy(proxy);
+
+        // Инициализация JDA с использованием прокси
+        jda = JDABuilder.createDefault(properties.getToken())
                 .disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE, CacheFlag.ACTIVITY, CacheFlag.STICKER, CacheFlag.ROLE_TAGS, CacheFlag.FORUM_TAGS, CacheFlag.ONLINE_STATUS, CacheFlag.CLIENT_STATUS)
                 .setBulkDeleteSplittingEnabled(false)
                 .setCompression(Compression.NONE)
                 .setActivity(Activity.playing("собачьи дела"))
                 .setStatus(OnlineStatus.ONLINE)
-                .enableIntents(GatewayIntent.GUILD_MESSAGES)
-                .enableIntents(GatewayIntent.DIRECT_MESSAGES)
-                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                .enableIntents(GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES, GatewayIntent.MESSAGE_CONTENT)
                 .addEventListeners(new MessageListener())
                 .setEnableShutdownHook(true)
                 .setAutoReconnect(true)
+                .setHttpClient(httpClientBuilder.build()) // Передаём кастомный HTTP клиент
+                .setWebsocketFactory(new WebSocketFactory() {
+                    @Override
+                    public WebSocket createSocket(String url) throws IOException {
+                        WebSocketFactory factory = new WebSocketFactory();
+                        factory.setVerifyHostname(false);  // ОТКЛЮЧАЕТ ПРОВЕРКУ HOSTNAME
+                        return factory.createSocket(url);
+                    }
+                })
                 .build();
     }
 
